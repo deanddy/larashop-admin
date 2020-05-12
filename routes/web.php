411c75e5783964b35319572d0dae6f4ae86164a7
Rizki Dendy <rizkidendy59@gmail.com>
2020-05-12 17:05:41 +0700

    Layout, Halaman dan Styling

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
+Route::match(['get', 'post'], '/register', function () {
+    return redirect("/login");
+})->name("register");
 
 Route::get('/home', 'HomeController@index')->name('home');
